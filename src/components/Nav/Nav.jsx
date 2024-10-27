d3abac5cdf88d8b7540d6afce1aa2353efb01a82
if3ll <sunnwydays@gmail.com>
2024-10-26 23:07:00 -0400

    Remove safety page and add about page

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,12 +6,12 @@ const Nav = () => {
     return (
         <div className = "nav">
             <Link to="/">
-                <img src={logo} alt='Dont Die, Stay Safe logo'/>
+                <img src={logo} alt='Safelink logo'/>
             </Link>
             <span className="nav-container">
                 <Link to="/report" className="nav-link"><p>Report</p></Link>
-                <Link to="/safety" className="nav-link"><p>Safety</p></Link>
                 <Link to="/volunteer" className="nav-link"><p>Volunteer</p></Link>
+                <Link to="/about" className="nav-link"><p>About</p></Link>
             </span>
         </div>   
     );
