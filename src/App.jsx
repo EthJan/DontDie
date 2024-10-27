70f463392b45546ea3c171c5e5ab793597ae7159
if3ll <sunnwydays@gmail.com>
2024-10-27 00:18:19 -0400

    Add about page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from "./components/Nav/Nav";
 import Home from "./components/pages/Home/Home"
 import Report from "./components/pages/Report/Report"
 import Volunteer from "./components/pages/Volunteer/Volunteer"
-// import About from "./components/pages/About/About"
+import About from "./components/pages/About/About"
 
 export default function App() {
   return (
@@ -14,7 +14,7 @@ export default function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/report" element={<Report/>} />
         <Route path="/volunteer" element={<Volunteer/>} />
-        {/* <Route path="/about" element={<About/>} /> */}
+        <Route path="/about" element={<About/>} />
         <Route path="*" element={<Home/>} />
       </Routes>
     </Router>
---
+++ src/components/pages/About/About.jsx
@@ -0,0 +1,17 @@
+import teamImage from '../../../assets/team.jpg';
+import './About.css';
+
+const About = () => {
+    return (
+        <div className="about">
+        <h1>About Safelink</h1>
+            <p>Over the past 50 years, the number of disasters worldwide experienced a fivefold increase, affecting millions of people each year. These natural disasters come in different ways and come without warning, leaving communities in urgent need of resources, aid, and volunteers. That’s where the idea for Safelink came from. We realized there was a need for a platform connecting everyday citizens to critical information about natural disasters. 
+            Introduce Safelink:
+            Our app provides a user-friendly platform that connects people to critical information and volunteering opportunities in real-time. Whether you're affected by an event or ready to lend a helping hand, Safelink makes it easy to see, report, and respond to natural disasters around the world.
+            </p>
+            <img src={teamImage} alt="Safelink" width="500" height="200" />
+        </div>
+    );
+}
+
+export default About;
