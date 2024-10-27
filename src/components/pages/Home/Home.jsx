0ff33a0bb71c53a796c6767e3fbdd31c1d27c588
Ethan <ethanbjan@gmail.com>
2024-10-26 22:20:48 -0400

    Map changes. updated bugs

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './Home.css';
 import iconUrl from '../../../assets/wildfire.png';
 
+
 function Home() {
 	const [locations, setLocations] = useState([]);
 	const [error, setError] = useState(null);
@@ -31,36 +32,56 @@ function Home() {
 		}
 
 		function initMap() {
-			const mapOptions = {
-				// Define Starting position 
-				center: { lat: 43.6598, lng: -79.3973 },
-				zoom: 3,
+			// Define the boundaries for soft boundary 
+			const verticalBounds = {
+			  north: 80.0,  
+			  south: -80.0,  
+			  west: -180.0,
+			  east: 180.0,  
 			};
+					  
+			const mapOptions = {
+			  center: { lat: 40.0, lng: -95.0 }, // Initial map center
+			  zoom: 5,
+			  minZoom: 3,
+			  zoomControl: true,
+			  zoomControlOptions: {
+				position: window.google.maps.ControlPosition.TOP_LEFT,
+			  },
+			  restriction: {
+				latLngBounds: verticalBounds,
+				strictBounds: false, 
+			  },
+			  gestureHandling: "greedy" // Removes page scrolling and enables map scrolling
+			};
+		  
 			const map = new window.google.maps.Map(document.getElementById('map'), mapOptions);
+		  
 
-			// For each disastor entry, pin it on the map
 			locations.forEach(location => {
-				const marker = new window.google.maps.Marker({
-					position: { lat: location.lat, lng: location.lng },
-					map: map,
-
-					// Add if statement and assign proper icon accordingly
-					icon: {
-						url: iconUrl,
-						// Adjust size for scale
-						scaledSize: new window.google.maps.Size(100, 100),
-					}
-				});
-				// Window for each pin
-				const infoWindow = new window.google.maps.InfoWindow({
-					content: `<h3>${location.lat}</h3><p>${location.lng}</p>`,
-				});
-
-				marker.addListener('click', () => {
-					infoWindow.open(map, marker);
-				});
+			  const marker = new window.google.maps.Marker({
+				position: { lat: location.lat, lng: location.lng },
+				map: map,
+				title: location.title,
+				icon: {
+				  url: iconUrl,
+				  scaledSize: new window.google.maps.Size(100, 100),
+				},
+			  });
+		  
+			  const infoWindow = new window.google.maps.InfoWindow({
+				content: `<h3>${location.title}</h3><p>${location.description}</p>`,
+			  });
+		  
+			  marker.addListener('click', () => {
+				infoWindow.open(map, marker);
+			  });
 			});
-		}
+			// Load window up after 
+			window.onload = initMap;
+		  }
+		  
+			  
 	}, [locations]);
 
 	return (
@@ -72,6 +93,7 @@ function Home() {
 			)}
 		</div>
 	);
+	
 }
 
 export default Home;
