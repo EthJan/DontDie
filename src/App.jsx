165c7ff7ce6fa6d103ab37e584de06e7b0ad7d13
if3ll <sunnwydays@gmail.com>
2024-10-26 23:33:33 -0400

    Add report form

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Home from "./components/pages/Home/Home"
+import Report from "./components/pages/Report/Report"
 import Volunteer from "./components/pages/Volunteer/Volunteer"
+// import About from "./components/pages/About/About"
 
 export default function App() {
   return (
@@ -10,8 +12,9 @@ export default function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home/>} />
-        {/* <Route path="/report" element={<Report/>} /> */}
+        <Route path="/report" element={<Report/>} />
         <Route path="/volunteer" element={<Volunteer/>} />
+        {/* <Route path="/about" element={<About/>} /> */}
         <Route path="*" element={<Home/>} />
       </Routes>
     </Router>
