b810975f655a543bedc4eff296f36102173da707
Eric Wang <ERICIRIS1979@icloud.com>
2024-10-26 22:01:27 -0400

    Add filtering, fix api issue, add address to database

--- python/geocode.py
+++ python/geocode.py
@@ -1,21 +1,32 @@
 import requests
-from flask import jsonify
+#import logging
 
-GEOCODING_API_KEY = "AIzaSyCLHKPgYJjtjPOSWtq3XFbrcrE--5MTUUU"
+GEOCODING_API_KEY = "AIzaSyCZJxjBwsEYfsZSnpMyCffg-4I3HMesCkM"  # Replace with your actual API key
 
 def geocode_address(address):
     geocoding_url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={GEOCODING_API_KEY}"
-
+    #try:
     response = requests.get(geocoding_url)
+    response.raise_for_status()
     geocode_data = response.json()
 
-    #check if the response contains results
-    if geocode_data["status"] != "OK" or not geocode_data["results"]:
-        return jsonify({"error": "Could not geocode the provided address"}), 400
+    #logging.debug(f"Geocode API response: {geocode_data}")
+    #print("Geocode API response:", geocode_data)
 
-    #get latitude and longitude
+    # Check if the response contains results
+    if geocode_data["status"] != "OK" or not geocode_data["results"]:
+        logging.error("Geocoding API did not return OK status or results are empty.")
+        return None
+
+    # Correct extraction of latitude and longitude from the JSON response
     location = geocode_data["results"][0]["geometry"]["location"]
     latitude = location["lat"]
     longitude = location["lng"]
-
     return {"latitude": latitude, "longitude": longitude}
+    # except requests.exceptions.RequestException as e:
+    #     logging.error(f"RequestException in geocode_address: {e}")
+    #     return None
+    # except KeyError:
+    #     logging.error("KeyError in parsing geocode response")
+    #     return None
+
